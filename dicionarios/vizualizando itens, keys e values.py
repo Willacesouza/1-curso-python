ba449342e625f219164b20e99394e0012a911996
Willacesouza <168701470+Willacesouza@users.noreply.github.com>
2024-08-21 19:18:53 -0400

    21/08/24

---
+++ dicionarios/vizualizando itens, keys e values.py
@@ -0,0 +1,23 @@
+aluno = {
+    "nome": "willace",
+    "idade": 14,
+    "aprovação": True,
+    "materias": ["fisica", "matematica", "inglish"]
+}
+
+print (aluno)
+
+#mostra so o nome do meu dicionario
+print (aluno.get("nome"))
+
+#diz quantas keys tem no dic
+print (len(aluno))
+
+#so aparece as chaves
+print (aluno.keys())
+
+#so aparece os valores
+print (aluno.values())
+
+#aparece os itens
+print(aluno.items())
